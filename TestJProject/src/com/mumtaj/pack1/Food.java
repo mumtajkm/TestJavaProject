a96fcf0f380ac91627e03208d7ddf21b11d07f44
mumtajkm <mumtaj.kjm@gmail.com>
2018-08-05 19:53:11 -0400

    fixed package name

---
+++ TestJProject/src/com/mumtaj/pack1/Food.java
@@ -0,0 +1,20 @@
+package com.mumtaj.pack1;
+
+public class Food {
+	
+	public void breakfast()
+	{
+		System.out.println("fruits");
+	}
+	
+	public void lunch()
+	{
+		System.out.println("sandwich");
+	}
+	
+	public void dinner()
+	{
+		System.out.println("salad");
+	}
+
+}
