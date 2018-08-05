9adfba970ebebc137755f668918eb3497063803a
mumtajkm <42117968+mumtajkm@users.noreply.github.com>
2018-08-05 18:49:26 -0400

    Add files via upload

---
+++ TestJProject/Food.java
@@ -0,0 +1,20 @@
+package com.mumtaj.pack3;
+
+public class Food {
+	
+	public void breakfast()
+	{
+		System.out.println("fruits");
+	}
+	
+	public void lunch()
+	{
+		System.out.println("sandwich");
+	}
+	
+	public void dinner()
+	{
+		System.out.println("salad");
+	}
+
+}
