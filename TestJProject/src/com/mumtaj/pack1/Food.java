2e631848fb7290e067336293648cab0f4473d9e3
mumtajkm <mumtaj.kjm@gmail.com>
2018-08-05 20:49:52 -0400

    testing if it gets committed

--- TestJProject/src/com/mumtaj/pack1/Food.java
+++ TestJProject/src/com/mumtaj/pack1/Food.java
@@ -1,44 +1,5 @@
-<<<<<<< HEAD
+
 package com.mumtaj.pack1;
-
-public class Food {
-	
-	public void breakfast()
-	{
-		System.out.println("fruits");
-	}
-	
-	public void lunch()
-	{
-		System.out.println("sandwich");
-	}
-	
-	public void dinner()
-	{
-		System.out.println("salad with olive oil");
-=======
-package com.mumtaj.pack3;
-
-public class Food {
-	
-	public void breakfast()
-	{
-		System.out.println("fruits");
-	}
-	
-	public void lunch()
-	{
-		System.out.println("sandwich");
-	}
-	
-	public void dinner()
-	{
-		System.out.println("salad");
->>>>>>> refs/remotes/origin/master
-	}
-
-}
-package com.mumtaj.pack3;
 
 public class Food {
 	
